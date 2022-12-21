755acb15f3aa37155b624236ddcff9796f55cb07
Manipal Sidhu <90776968+manipalsidhu@users.noreply.github.com>
2022-12-21 16:48:56 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,50 +16,50 @@ navLinks.forEach(link => {
 let number = document.getElementById("HTMLnumber");
 let counter = 0;
 setInterval(() => {
-    if (counter == 90) {
+    if (counter == 95) {
         clearInterval();
     }
     else {
         counter += 1;
         number.innerHTML = counter + "%";
     }
-}, 22);
+}, 21);
 
 //CSS bar Script
 let cssnumber = document.getElementById("cssnumber");
 let csscounter = 0;
 setInterval(() => {
-    if (csscounter == 80) {
+    if (csscounter == 90) {
         clearInterval();
     }
     else {
         csscounter += 1;
         cssnumber.innerHTML = csscounter + "%";
     }
-}, 25);
+}, 22);
 
 //JS bar Script
 let jsnumber = document.getElementById("jsnumber");
 let jscounter = 0;
 setInterval(() => {
-    if (jscounter == 65) {
+    if (jscounter == 80) {
         clearInterval();
     }
     else {
         jscounter += 1;
         jsnumber.innerHTML = jscounter + "%";
     }
-}, 30);
+}, 25);
 
 //num bar Script
 let numnumber = document.getElementById("number");
 let numcounter = 0;
 setInterval(() => {
-    if (numcounter == 60) {
+    if (numcounter == 80) {
         clearInterval();
     }
     else {
         numcounter += 1;
         numnumber.innerHTML = numcounter + "%";
     }
-}, 34);
+}, 25);
